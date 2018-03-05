7ee3caa8ea82bbdfe46e229ac7de7ac07826f930
Arturo Cordova <atu.cr92@gmail.com>
2018-03-05 15:10:01 -0600

    first

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-//dependencies required for the app
+// dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-//render css files
+//render css
 app.use(express.static("public"));
+// initial added tasks
+var task = ["Advanced Asynchronous Javascript", "Functional Javascript"];
+// Initial completed task
+var complete = ["Solid Principles", "Hexagonal Architecture"];
 
-//placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
-//placeholders for removed task
-var complete = ["finish jquery"];
+// get pending tasks
+app.get("/pendings", function(req, res) {
+    res.send(task);
+});
 
-//post route for adding new task 
-app.post("/addtask", function(req, res) {
-    var newTask = req.body.newtask;
-    //add the new task from the post route
+// Add new task
+app.post("/pendings", function(req, res) {
+    const newTask = req.body.newtask;
     task.push(newTask);
+    res.send(task);
+});
+
+// // get completed tasks
+app.get("/completes", function(req, res) {
+    res.send(complete);
+});
+
+// remove task
+app.post("/completes", function(req, res) {
+    const completeTask = req.body.check;
+    handleSelections(completeTask);
     res.redirect("/");
 });
 
-app.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        //check if the completed task already exits in the task when checked, then remove it
-        task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
+function handleSelections(completeTask){
+    // If are removing several tasks
+    if (typeof completeTask === "object") {
+        for (let i = 0; i < completeTask.length; i++) {
             complete.push(completeTask[i]);
             task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
-    res.redirect("/");
-});
-
-//render the ejs and display added task, completed task
+    // Check if remove one task
+    else{
+        complete.push(completeTask);
+        //check if the completed task already exits in the task when checked, then remove it
+        task.splice(task.indexOf(completeTask), 1);
+    }
+}
+//render the ejs
 app.get("/", function(req, res) {
     res.render("index", { task: task, complete: complete });
 });
-
 //set app to listen on port 3000
 app.listen(3000, function() {
     console.log("server is running on port 3000");
